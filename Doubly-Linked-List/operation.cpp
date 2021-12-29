b372c5d3c91f31f97843a2fa1864d397f1b49c32
Hetu Patel <hetu200211@gmail.com>
2021-12-29 14:00:54 +0530

    doubly

---
+++ Doubly-Linked-List/operation.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* prev;
+
+    node(int val){
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+void insertAtHead(node* &head,int val){
+    node* n = new node(val);
+    if(head!=NULL){
+    head->prev = n;
+    }
+    n->next = head;
+    head=n;
+}
+void insertAtTail(node* &head,int val){
+    node* n = new node(val);
+    if(head==NULL){
+        insertAtHead(head,val);
+        return;
+    }
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+}
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<"\n";
+}
+void deleteAtHead(node* &head){
+    node* temp = head;
+    head = head->next;
+    head->prev = NULL;
+    delete temp;
+
+}
+void deletion(node* &head,int pos){
+    if(pos==1){
+        deleteAtHead(head);
+        return;
+    }
+    node* temp = head;
+    int c = 1;
+    while(temp->next!=NULL&&c!=pos){
+        c++;
+        temp = temp->next;
+    }
+    if(temp->next!=NULL){
+    temp->next->prev = temp->prev;
+    }
+    temp->prev->next = temp->next;
+    delete temp;
+}
+int main(){
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    insertAtHead(head,5);
+    deletion(head,2);
+    display(head);
+}
